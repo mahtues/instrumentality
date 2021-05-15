067fab06fa14c33c9a25d2ffcd7866692a33a52c
mahtues <14203456+mahtues@users.noreply.github.com>
2021-05-15 18:02:02 -0300

    creating accounts.Repository interface

--- account/accounts.go
+++ account/accounts.go
@@ -2,20 +2,19 @@ package account
 
 import (
 	"context"
-	"log"
-
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/pkg/errors"
 	"golang.org/x/crypto/bcrypt"
 )
 
-type MongoModel struct {
-	Username string `bson:"username"`
-	Hash     string `bson:"hash"`
-	Email    string `bson:"email"`
+type Username string
+type Hash string
+type Email string
+
+type Account struct {
+	Username Username
+	Hash     Hash
+	Email    Email
 }
 
 type CreateForm struct {
@@ -29,24 +28,11 @@ type VerifyForm struct {
 	Password string `form:"password"`
 }
 
-func getClient(ctx context.Context) (*mongo.Client, error) {
-	return mongo.Connect(ctx, options.Client().ApplyURI("mongodb://mongodb:27017"))
-}
-
-func getCollection(ctx context.Context) (*mongo.Collection, error) {
-	client, err := getClient(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	return client.Database("instrumentality").Collection("accounts"), err
-}
+var defaultRepository, repoErr = NewMongoRepository(context.Background())
 
 func Create(ctx context.Context, form CreateForm) error {
-	accounts, err := getCollection(ctx)
-	if err != nil {
-		log.Println("connect error:", err.Error())
-		return err
+	if defaultRepository == nil {
+		return errors.Wrap(repoErr, "missing repository")
 	}
 
 	hash, err := bcrypt.GenerateFromPassword([]byte(form.Password), bcrypt.MinCost)
@@ -54,30 +40,21 @@ func Create(ctx context.Context, form CreateForm) error {
 		return err
 	}
 
-	account := MongoModel{Username: form.Username, Hash: string(hash), Email: form.Email}
+	account := Account{Username: Username(form.Username), Hash: Hash(hash), Email: Email(form.Email)}
 
-	_, err = accounts.Indexes().CreateOne(ctx, mongo.IndexModel{Keys: bson.M{"username": 1}, Options: options.Index().SetUnique(true)})
-	if err != nil {
-		return err
-	}
-
-	_, err = accounts.InsertOne(ctx, account)
-
-	return err
+	return defaultRepository.Create(ctx, account)
 }
 
 func Verify(ctx context.Context, form VerifyForm) error {
-	accounts, err := getCollection(ctx)
-	if err != nil {
-		log.Println("connect error:", err.Error())
-		return err
+	if defaultRepository == nil {
+		return errors.Wrap(repoErr, "missing repository")
 	}
 
-	filter := bson.D{{"username", form.Username}}
+	var account Account
+	var err error
 
-	var account MongoModel
-	if err := accounts.FindOne(ctx, filter).Decode(&account); err != nil {
-		return errors.Wrap(err, "invalid username")
+	if account, err = defaultRepository.FindByUsername(ctx, Username(form.Username)); err != nil {
+		return err
 	}
 
 	err = bcrypt.CompareHashAndPassword([]byte(account.Hash), []byte(form.Password))
---
+++ account/repo_mongo.go
@@ -0,0 +1,73 @@
+package account
+
+import (
+	"context"
+	"os"
+
+	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoAccount struct {
+	Username string `bson:"username"`
+	Hash     string `bson:"hash"`
+	Email    string `bson:"email"`
+}
+
+type MongoRepository struct {
+	client     *mongo.Client
+	database   *mongo.Database
+	collection *mongo.Collection
+}
+
+func NewMongoRepository(ctx context.Context) (*MongoRepository, error) {
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGODB_HOST")))
+	if err != nil {
+		return nil, err
+	}
+	database := client.Database("instrumentality")
+	collection := database.Collection("accounts")
+
+	_, err = collection.Indexes().CreateOne(ctx, mongo.IndexModel{Keys: bson.M{"username": 1}, Options: options.Index().SetUnique(true)})
+	if err != nil {
+		// todo: disconnect
+		return nil, err
+	}
+
+	return &MongoRepository{client, database, collection}, nil
+}
+
+func (m *MongoRepository) Create(ctx context.Context, account Account) error {
+	_, err := m.collection.InsertOne(ctx, encode(account))
+	return err
+}
+
+func (m *MongoRepository) FindByUsername(ctx context.Context, username Username) (Account, error) {
+	var mongoAccount MongoAccount
+	if err := m.collection.FindOne(ctx, bson.D{{"username", username}}).Decode(&mongoAccount); err != nil {
+		return Account{}, errors.Wrap(err, "invalid username")
+	}
+	return decode(mongoAccount), nil
+}
+
+func (m *MongoRepository) FindByEmail(_ context.Context, _ Email) (Account, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func encode(account Account) MongoAccount {
+	return MongoAccount{
+		Username: string(account.Username),
+		Hash:     string(account.Hash),
+		Email:    string(account.Email),
+	}
+}
+
+func decode(mongoAccount MongoAccount) Account {
+	return Account{
+		Username: Username(mongoAccount.Username),
+		Hash:     Hash(mongoAccount.Hash),
+		Email:    Email(mongoAccount.Email),
+	}
+}
